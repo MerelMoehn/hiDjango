5e493708b39bf45c12038e07b1575cfd2076743d
MerelMoehn <merelmoehn@outlook.com>
2022-12-16 15:33:38 +0000

    fix method

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['git.heroku.com/msmoehn-django-todo-app.git']
+ALLOWED_HOSTS = ['msmoehn-django-todo-app.herokuapp.com/"']
 
 
 # Application definition
