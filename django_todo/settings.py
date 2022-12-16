995bdc0d7339bd911b1a04256930a590aaf96886
MerelMoehn <merelmoehn@outlook.com>
2022-12-16 15:30:11 +0000

    fix

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@69uacw$8c2bghte*3*8d566dr&51aond_5)xzdr_)6!i6*r1v'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['git.heroku.com/msmoehn-django-todo-app.git']
 
 
 # Application definition
